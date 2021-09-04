c12537ffa25647a7a994e5be6b67b9f1b9365351
matheusv11 <mathsales360@gmail.com>
2021-09-04 00:16:40 -0300

    Adição de crons

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ const puppeteer = require("puppeteer");
 const axios     = require('axios');
 const fs        = require('fs');
 const path      = require('path');
+const cron      = require('node-cron');
 
-
-// (async function sec(){
-//     const url = "https://www.mensagensdebomdia.com.br/wp-content/uploads/2020/01/77B38E74-EE06-4EB2-8A63-9031E494337C.jpeg";
-//     const opts = {
-//         responseType: "arraybuffer"
-//     };
-//     const {data: buffer} = await axios.get(url, opts);
-//     console.log(buffer);
-//     //Chamar função dentro dela mesmo
-//TIRAR PRINT DO QR CODE
-// })();
-
-// return;
-(async function main() {
+const sendMessage = async(bom) => {
 
   try {
 
@@ -30,29 +18,23 @@ const path      = require('path');
     //GO TO ZAP
     await page.goto("https://web.whatsapp.com/");
 
-    // await page.waitForSelector("._1MXsz"); //BLACK
-    await page.waitForSelector("._1XkO3");    //WHITE
-    // await page.waitForTimeout(5000);
+    await page.waitForSelector("._1XkO3");    //WHITE THEME
 
     //SEARCH CONVERSATION IN ZAP
-    // const contact = "+55 89 8819-1796";
     const contact = "PATO LÓGICO 🇧🇷 🇮🇱 🇺🇸 🇦🇴 🇰🇵 🇨🇳";
     await page.click(`span[title='${contact}']`);
     await page.waitForSelector("._1fqrG");
 
-    //FOCUS ON MENSAGE BAR
+    //FOCUS ON MESSAGE BAR
     await page.focus("div[data-tab='6']"); 
     
     await page.evaluate(() => {
       const message = "@Paulo Isaac";
       document.execCommand("insertText", false, message);
-    });
+    }); //TROCAR EVALUATE
 
     await page.waitForTimeout(1000);
-    console.log(await page.$("._3lPuS"));
-
-    return;
-    await page.click("span[data-testid='send']");
+    await page.click(".matched-text");
     await page.waitForTimeout(500);
 
     //OPEN CLIPBOARD
@@ -61,27 +43,20 @@ const path      = require('path');
 
     //INPUT FILE
     const inputFile = await page.$('input[accept="image/*,video/mp4,video/3gpp,video/quicktime"]');
-    await inputFile.uploadFile( path.resolve(__dirname,"bom.jpg") )
-    
+    await inputFile.uploadFile( path.resolve(__dirname, `${bom}.jpg`) );
+
     //SEND FILE
     await page.waitForTimeout(2000);
     await page.click('span[data-testid="send"]');
     await page.waitForTimeout(3000);
 
     await browser.close();
+
   } catch (e) {
     console.error("ERROR=>", e);
   }
 
-})();
+}
 
-// (async function ok(){
-//   let fet = await fetch("https://www.mensagensdebomdia.com.br/wp-content/uploads/2020/01/77B38E74-EE06-4EB2-8A63-9031E494337C.jpeg");
-//   let buffer = await fet.arrayBuffer();
-//   console.log(buffer)
-
-// } )()
-
-//Poderia usar o fs para ler a imagem
-//Poderia requisitar a imagem da url e salvar local
-//Poderia salvar local a imagem e salvar pra então o fs ler e jogar no clipboard
+cron.schedule('0 6 * * *', ()=> sendMessage('dia')   );
+cron.schedule('0 0 * * *', ()=> sendMessage('noite') );
